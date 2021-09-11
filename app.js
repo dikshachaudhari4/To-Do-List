f29c9ea717cd1288ddff21ac86363a51ffb16020
diksha chaudhari <chaudharidiksha4@gmail.com>
2021-09-11 16:23:10 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,78 @@
+var express=require("express");
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var app=express();
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+const mongoDbURL = process.env.MONGODB_URL || "mongodb://localhost:27017/todolistDB"
+mongoose.connect(mongoDbURL,{useNewUrlParser:true,useUnifiedTopology:true});
+const itemSchema={
+    name:String
+}
+const Item=mongoose.model("Item",itemSchema);
+const item1=new Item({
+    name:"First Item",
+});
+const d=[item1];
+app.get("/",function(req,res)
+{
+   Item.find({},function(err,f)
+   {
+      if(f.length===0)
+      {
+        Item.insertMany(d,function(err)
+        {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Successfully saved items to DB");
+            }
+        });
+      res.redirect("/");
+      }
+      else{
+      res.render("list",{newListItems:f});
+      }
+   })
+  ;
+})
+app.post("/add",function(req,res)
+{
+     const itemName=req.body.n;
+    const item=new Item({
+       name:itemName
+   });
+item.save();
+res.redirect("/");
+});
+
+app.post("/delete",function(req,res)
+{
+  const check=req.body.checkbox;
+  Item.findByIdAndRemove(check,function(err)
+  {
+      if(!err)
+      {
+          console.log("Successfully deleted");
+          res.redirect("/");
+      }
+  });
+});
+app.post("/deleteAll", function (req, res) {
+    Item.deleteMany({}, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Deleted all");
+      }
+    });
+    res.redirect("/");
+  });  
+app.listen(3000,function()
+{
+    console.log("Server is listening to port 3000");
+})
+
